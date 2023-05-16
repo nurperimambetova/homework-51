76d7173f83cbed353323254b4874f0fd9377f314
Nurperi Mambetova <nurperimambetova1@gmail.com>
2023-05-16 22:49:42 +0200

    Created the component and design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Circle from "./Circle/Circle";
 
-const App = () => (
-    <div className="App">
-        <p>Hello</p>
-    </div>
-);
+const App = () => {
+    const [numbers, setNumbers] = useState<number[]>([]);
+
+    const changeNumbers = () => {
+
+
+    }
+
+    return (
+        <div className="App">
+            <button onClick={changeNumbers} className="changeBtn">New numbers</button>
+            <div className="circles">
+                <Circle number={0} />
+                <Circle number={1} />
+                <Circle number={2} />
+                <Circle number={3} />
+                <Circle number={4} />
+            </div>
+
+        </div>
+    );
+};
 
 export default App;
